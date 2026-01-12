9f9ce4fd5e448555ce05dfd6ca200bdba9073d1c
sstanley45 <stanleynjuguna445@gmail.com>
2026-01-12 20:41:19 +0300

    Commented events on hooks

--- songa_mobility_customizations/hooks.py
+++ songa_mobility_customizations/hooks.py
@@ -138,22 +138,22 @@ app_license = "mit"
 # Hook on document methods and events
 
 doc_events = {
-    "Purchase Order": {
-        "before_submit": "songa_mobility_customizations.services.rest.validate_purchase_order"
-    },
+    # "Purchase Order": {
+    #     "before_submit": "songa_mobility_customizations.services.rest.validate_purchase_order"
+    # },
 
-    "Material Request": {
-        "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_material_request_workflow.handle_material_request_workflow"
-    },
-    "Stock Entry": {
-        "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_stock_entry_workflow.handle_stock_entry_workflow"
-    },
-    "Purchase Order": {
-        "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_purchase_order_workflow.handle_purchase_order_workflow"
-    },
-    "Purchase Invoice": {
-        "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_purchase_invoice_workflow.handle_purchase_invoice_workflow"
-    },
+    # "Material Request": {
+    #     "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_material_request_workflow.handle_material_request_workflow"
+    # },
+    # "Stock Entry": {
+    #     "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_stock_entry_workflow.handle_stock_entry_workflow"
+    # },
+    # "Purchase Order": {
+    #     "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_purchase_order_workflow.handle_purchase_order_workflow"
+    # },
+    # "Purchase Invoice": {
+    #     "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_purchase_invoice_workflow.handle_purchase_invoice_workflow"
+    # },
     # "Sales Invoice": {
     #     "on_update": "songa_mobility_customizations.services.workflow_handlers.handle_sales_invoice_workflow.handle_sales_invoice_workflow"
     # },
